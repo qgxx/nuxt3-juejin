d2beecff67e606e283f7a6242d0d5441e7d4e72f
qgxx <qgxx2825313915@outlook.com>
2023-02-10 10:36:19 +0800

    naive-ui

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
+<script setup>
+import { NButton } from "naive-ui"
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <n-button type="primary" size="medium">click</n-button>    
   </div>
 </template>
