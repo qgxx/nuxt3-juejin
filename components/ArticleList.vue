03b57535561c445eb90c6b5c3b88a8ae51066e6f
qgxx <qgxx2825313915@outlook.com>
2023-02-22 12:14:14 +0800

    arc

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -17,31 +17,37 @@ onMounted(() => {
   <div class="artic-list">
     <template v-for="arc in articleList.articList">
       <div class="arc-item">
-        <div class="title">
+        <div class="item-header">
+          <div class="arc-auther">
+            {{ arc.auther }}
+          </div>
+          <div class="arc-cat">
+            <ul>
+                <li v-for="cat in arc.category" class="cat-item">{{ cat }}</li>
+            </ul>
+          </div>
+          <div class="arc-data">
+            {{ arc.date }}
+          </div>
+        </div>
+        <div class="item-mid">
+          <div class="arc-title">
             {{ arc.title }}
+          </div>
+          <div class="arc-desc">
+            {{ arc.describe }}
+          </div>
         </div>
-        <div class="arc-cat">
-           <ul>
-              <li v-for="cat in arc.category">{{ cat }}</li>
-           </ul>
-        </div>
-        <div class="arc-data">
-          {{ arc.date }}
-        </div>
-        <div class="arc-title">
-          {{ arc.title }}
-        </div>
-        <div class="arc-desc">
-          {{ arc.describe }}
-        </div>
-        <div class="arc-view">
-          {{ arc.view }}
-        </div>
-        <div class="arc-like">
-          {{ arc.like }}
-        </div>
-        <div>
-          {{ arc.comment }}
+        <div class="item-footer">
+          <div class="arc-view">
+            {{ arc.view }}
+          </div>
+          <div class="arc-like">
+            {{ arc.like }}
+          </div>
+          <div>
+            {{ arc.comment }}
+          </div>
         </div>
       </div>
     </template>
@@ -51,7 +57,10 @@ onMounted(() => {
 <style scoped lang="scss">
 .artic-list {
     .arc-item {
-        border-top: 1px solid #7f7f7f;
+      position: relative;
+      border-top: 1px solid #7f7f7f;
+      height: 10em;
+      overflow-y: hidden;
     }
 }
 </style>
